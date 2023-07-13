aebd94cdfd46fc48c2fe70e430653055f32324b2
aayu2002 <91870028+aayu2002@users.noreply.github.com>
2023-07-13 22:16:15 +0530

    6.py

---
+++ 6.py
@@ -0,0 +1,14 @@
+def contains_duplicate(nums):
+
+
+  seen = set()
+  for num in nums:
+    if num in seen:
+      return True
+    else:
+      seen.add(num)
+  return False
+nums = [1, 2, 3, 1]
+contains_duplicate(nums)
+
+
